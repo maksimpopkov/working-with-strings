a0a253293586acb9485e4f4c429a71833d386716
Alexander Rykov <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-10 15:25:06 +0300

    Fix S2234 warning.

--- WorkingWithStrings/ConcatenatingStrings.cs
+++ WorkingWithStrings/ConcatenatingStrings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.VisualBasic;
 
 namespace WorkingWithStrings
 {
@@ -18,9 +19,7 @@ namespace WorkingWithStrings
         /// </summary>
         public static string ConcatenateStrings(string str1, string str2, string str3)
         {
-            // TODO #7-1. Analyze unit tests for the method, and add the method implementation.
-            // Use String.Concat method to concatenate string: https://docs.microsoft.com/en-us/dotnet/api/system.string.concat
-            throw new NotImplementedException();
+            return string.Concat(str1, str2, str3);
         }
 
         /// <summary>
@@ -28,9 +27,7 @@ namespace WorkingWithStrings
         /// </summary>
         public static string ConcatenateStrings(string str1, string str2, string str3, string str4)
         {
-            // TODO #7-2. Analyze unit tests for the method, and add the method implementation.
-            // Use String.Concat method to concatenate string: https://docs.microsoft.com/en-us/dotnet/api/system.string.concat
-            throw new NotImplementedException();
+            return string.Concat(str1, str2, str3, str4);
         }
 
         /// <summary>
@@ -38,9 +35,7 @@ namespace WorkingWithStrings
         /// </summary>
         public static string ConcatenateStrings(IEnumerable<string> strings)
         {
-            // TODO #7-3. Analyze unit tests for the method, and add the method implementation.
-            // Use String.Concat method to concatenate string: https://docs.microsoft.com/en-us/dotnet/api/system.string.concat
-            throw new NotImplementedException();
+            return string.Concat(strings);
         }
 
         /// <summary>
@@ -48,9 +43,7 @@ namespace WorkingWithStrings
         /// </summary>
         public static string ConcatenateValues(string str, int intValue, long longValue)
         {
-            // TODO #7-4. Analyze unit tests for the method, and add the method implementation.
-            // Use String.Concat method to concatenate string: https://docs.microsoft.com/en-us/dotnet/api/system.string.concat
-            throw new NotImplementedException();
+            return string.Concat(str, intValue, longValue);
         }
 
         /// <summary>
@@ -58,9 +51,7 @@ namespace WorkingWithStrings
         /// </summary>
         public static string ConcatenateValues(short shortValue, float floatValue, bool boolValue, double doubleValue)
         {
-            // TODO #7-5. Analyze unit tests for the method, and add the method implementation.
-            // Use String.Concat method to concatenate string: https://docs.microsoft.com/en-us/dotnet/api/system.string.concat
-            throw new NotImplementedException();
+            return string.Concat(shortValue, floatValue, boolValue, doubleValue);
         }
 
         /// <summary>
@@ -68,9 +59,7 @@ namespace WorkingWithStrings
         /// </summary>
         public static string ConcatenateValues(IEnumerable<object> values)
         {
-            // TODO #7-6. Analyze unit tests for the method, and add the method implementation.
-            // Use String.Concat method to concatenate string: https://docs.microsoft.com/en-us/dotnet/api/system.string.concat
-            throw new NotImplementedException();
+            return string.Concat(values);
         }
     }
 }
--- WorkingWithStrings/CreatingStrings.cs
+++ WorkingWithStrings/CreatingStrings.cs
@@ -17,9 +17,7 @@ namespace WorkingWithStrings
         /// </summary>
         public static string ReturnStringWithRepeatedChars(char c, int count)
         {
-            // TODO #2-1. Implement the method using String(Char, Int32) constructor
-            // See the constructor documentation page: https://docs.microsoft.com/en-us/dotnet/api/system.string.-ctor#System_String__ctor_System_Char_System_Int32_
-            throw new NotImplementedException();
+            return new string(c, count);
         }
 
         /// <summary>
@@ -27,9 +25,7 @@ namespace WorkingWithStrings
         /// </summary>
         public static string ReturnStringFromCharArray(char[] c)
         {
-            // TODO #2-2. Implement the method using String(Char[]) constructor
-            // See the constructor documentation page: https://docs.microsoft.com/en-us/dotnet/api/system.string.-ctor#System_String__ctor_System_Char___
-            throw new NotImplementedException();
+            return new string(c);
         }
 
         /// <summary>
@@ -37,9 +33,7 @@ namespace WorkingWithStrings
         /// </summary>
         public static string ReturnStringFromCharArray(char[] c, int startIndex, int length)
         {
-            // TODO #2-3. Implement the method using String(Char[], Int32, Int32) constructor
-            // See the constructor documentation page: https://docs.microsoft.com/en-us/dotnet/api/system.string.-ctor#System_String__ctor_System_Char___System_Int32_System_Int32_
-            throw new NotImplementedException();
+            return new string(c, startIndex, length);
         }
     }
 }
